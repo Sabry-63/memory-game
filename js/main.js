fe5820b3912772814bf4669133ce3152e4fb496b
Sabry-63 <52372174+Sabry-63@users.noreply.github.com>
2020-04-24 14:29:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Btn Start Game
-document.querySelector(".control-btn span").onclick = function() {
+function startGame() {
   const name = document.querySelector(".name span");
   // Name Is Empty && Set Name
   let yourName = prompt("Whats Your Name");
@@ -7,21 +7,55 @@ document.querySelector(".control-btn span").onclick = function() {
     ? (name.textContent = "Unknown")
     : (name.textContent = yourName);
   // Remove   Splash Screen
-  document.querySelector(".control-btn").remove();
+  document.querySelector(".control-btn").style.display = "none";
   show();
   stopClick();
-};
+  setTimeout(() => {
+    timer = setInterval(time, duration);
+  }, 3000);
+}
+
+// Click Start Game
+document.querySelector(".start").addEventListener("click", startGame);
+
+// Show All Cards For 1s
 function show() {
   let blocks = document.querySelectorAll(".game-block");
   blocks.forEach(block => {
     block.classList.add("show");
     setTimeout(() => {
       block.classList.remove("show");
-    }, duration);
+    }, 3000);
   });
 }
 
 let duration = 1000;
+
+// Timer Function
+function time() {
+  let min = document.querySelector(".min"),
+    scend = document.querySelector(".scend");
+
+  if (scend.textContent >= 59) {
+    min.textContent = 1;
+    scend.textContent = 0;
+    min.textContent < 10
+      ? (min.textContent = "0" + min.textContent)
+      : min.textContent;
+  } else {
+    scend.textContent = parseInt(scend.textContent) + 1;
+  }
+  scend.textContent < 10
+    ? (scend.textContent = "0" + scend.textContent)
+    : scend.textContent;
+  if (blocksContainer.classList.contains("done")) {
+    clearInterval(timer);
+    const win = document.querySelector(".box-win");
+    const yourTime = document.querySelector(".Your-time span");
+    yourTime.textContent = document.querySelector(".timer").textContent;
+    win.style.visibility = "visible";
+  }
+}
 
 let blocksContainer = document.querySelector(".memory-game-blocks");
 
@@ -34,7 +68,10 @@ shuffle(orderRange);
 
 // Add Oroder Css To Games Blocks
 blocks.map((block, index) => {
+  // console.log(blocksContainer.children.item(index).localName)
+
   block.style.order = orderRange[index];
+
   // Add Click Event
   block.addEventListener("click", function() {
     // Trigger The Filp Block Function
@@ -51,10 +88,19 @@ function filpBlock(selectedBlock) {
   let allFilppedBlocks = blocks.filter(filppedBlock =>
     filppedBlock.classList.contains("is-flipped")
   );
+
   if (allFilppedBlocks.length == 2) {
     stopClick();
 
     checkMatched(allFilppedBlocks[0], allFilppedBlocks[1]);
+  }
+
+  // Checked All Cards Has Class Match
+  let dones = blocks.filter(done => done.classList.contains("match"));
+
+  // End Game
+  if (dones.length == 20) {
+    blocksContainer.classList.add("done");
   }
 }
 
